e13851b1786d9acf7f8247c1e1e947662ff48a09
abhinavgupta1309 <guptaabhinav1309@gmail.com>
2025-03-18 11:05:05 +0530

    Address Book Setup

---
+++ src/main/java/com/example/AddressBookApp/AddressBookAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.AddressBookApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Contacts!!!");
+		SpringApplication.run(AddressBookAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/AddressBookApp/controller/ContactController.java
@@ -0,0 +1,26 @@
+package com.example.AddressBookApp.controller;
+
+import com.example.AddressBookApp.model.Contact;
+import com.example.AddressBookApp.service.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/contacts")
+public class ContactController {
+
+    @Autowired
+    private ContactService service;
+
+    @GetMapping
+    public List<Contact> getAllContacts() {
+        return service.getAllContacts();
+    }
+    @PostMapping("/add")
+    public Contact addContact(@RequestBody Contact contact) {
+        return service.saveContact(contact);
+    }
+
+}
