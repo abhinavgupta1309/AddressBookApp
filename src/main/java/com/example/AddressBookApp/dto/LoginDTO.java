8f429741eb7e0a8a2e46e5b5e64b854908e2b6fd
abhinavgupta1309 <guptaabhinav1309@gmail.com>
2025-03-18 15:23:26 +0530

    Implemented Lombok

--- src/main/java/com/example/AddressBookApp/dto/LoginDTO.java
+++ src/main/java/com/example/AddressBookApp/dto/LoginDTO.java
@@ -1,23 +1,35 @@
-package com.example.AddressBookApp.dto;
+//package com.example.AddressBookApp.dto;
+//
+//public class LoginDTO {
+//    private String email;
+//    private String password;
+//
+//    // Getters and Setters
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//}
 
+package com.example.AddressBookApp.dto;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginDTO {
     private String email;
     private String password;
 
-    // Getters and Setters
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
